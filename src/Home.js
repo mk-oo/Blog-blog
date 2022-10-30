749419ff99fea6c6584a156e280ca3fb1ee2f8a2
Mostafa karam <mostafakaram934@example.com>
2022-10-30 12:54:29 +0200

    catching errors and handling it

--- src/Home.js
+++ src/Home.js
@@ -6,18 +6,40 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error,setError] = useState(null);
     useEffect(() => {
 
+        // load the json from endpoint 
         const parsedJson = 
         fetch('http://localhost:8000/blogs')
-        .then(res=>{ return res.json();});
+        .then(res=>{
+            if (res.ok === true && res.status === 200){
 
-        parsedJson.then((data) =>{
+                return res.json();
+
+            }else{
+                throw Error('error in requesting data from the server ... ');
+            }
+
+            
+            });
+
+        //update the blogs state by useState Hook 
+        const listOfBlogs = parsedJson.then((data) =>{
 
             console.log(data);
             setBlogs(data);
             setIsLoading(false);
-        })
+            setError(null);
+
+        });
+
+        // catch errors of fetching The endpoint
+        listOfBlogs.catch(e=>{
+            console.log(e.message);
+            setIsLoading(false);
+            setError(e.message);
+        });
 
     },[])
 
@@ -26,6 +48,8 @@ const Home = () => {
     return ( 
 
         <div className="home">
+
+        {error && <div>{error}</div> }
 
         {isLoading && <div>loading .... </div> }
 
